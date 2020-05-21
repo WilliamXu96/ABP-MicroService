0febdd6b276770854030d9e077f1a875361cdb46
xuchengzhi <1091113110@qq.com>
2020-05-21 17:11:26 +0800

    完善用户管理模块

--- WebApp/vue-element-admin/src/views/user/index.vue
+++ WebApp/vue-element-admin/src/views/user/index.vue
@@ -35,14 +35,15 @@
           size="mini"
           type="primary"
           icon="el-icon-plus"
-          @click="save"
+          @click="handleCreate"
+          v-permission="['AbpIdentity.Users.Create']"
         >新增</el-button>
         <el-button
           class="filter-item"
           size="mini"
           type="success"
           icon="el-icon-edit"
-          @click="save"
+          @click="handleUpdate()"
         >修改</el-button>
         <el-button
           slot="reference"
@@ -55,7 +56,12 @@
       </div>
     </div>
     <!--表单渲染-->
-    <el-dialog append-to-body :close-on-click-modal="false" title="用户新增" width="570px">
+    <el-dialog
+      :visible.sync="dialogFormVisible"
+      :close-on-click-modal="false"
+      :title="formTitle"
+      width="570px"
+    >
       <el-form
         ref="form"
         :inline="true"
@@ -64,17 +70,20 @@
         size="small"
         label-width="66px"
       >
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="form.username" />
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="form.userName" />
         </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model.number="form.phone" />
+        <el-form-item label="电话" prop="phoneNumber">
+          <el-input v-model.number="form.phoneNumber" />
         </el-form-item>
-        <el-form-item label="昵称" prop="nickName">
-          <el-input v-model="form.nickName" />
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="form.name" />
         </el-form-item>
         <el-form-item label="邮箱" prop="email">
           <el-input v-model="form.email" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="form.password" />
         </el-form-item>
         <el-form-item label="性别">
           <el-radio-group v-model="form.gender" style="width: 178px">
@@ -82,46 +91,34 @@
             <el-radio label="女">女</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="状态">
-          <!-- <el-radio-group v-model="form.enabled" :disabled="form.id === user.id">
-                <el-radio
-                  v-for="item in dict.user_status"
-                  :key="item.id"
-                  :label="item.value"
-                >{{ item.label }}</el-radio>
-          </el-radio-group>-->
-        </el-form-item>
-        <el-form-item style="margin-bottom: 0;" label="角色" prop="roles">
-          <el-select
-            v-model="form.roles"
-            style="width: 437px"
-            multiple
-            placeholder="请选择"
-          >
-            <!-- <el-option
-                  v-for="item in roles"
-                  :key="item.name"
-                  :disabled="level !== 1 && item.level <= level"
-                  :label="item.name"
-                  :value="item.id"
-            />-->
+        <el-form-item label="角色" prop="roles">
+          <el-select v-model="checkedRole" style="width: 437px" placeholder="请选择">
+            <el-option
+              v-for="item in roleList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
           </el-select>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button type="text" @click="cancel">取消</el-button>
-        <el-button type="primary" @click="save">确认</el-button>
+        <el-button type="text" @click="dialogFormVisible = false">取消</el-button>
+        <el-button v-loading="formLoading" type="primary" @click="save">确认</el-button>
       </div>
     </el-dialog>
     <!--表格渲染-->
     <el-table
+      ref="multipleTable"
       v-loading="listLoading"
       :data="list"
       size="small"
-      highlight-current-row
       style="width: 100%;"
       @sort-change="sortChange"
+      @selection-change="handleSelectionChange"
+      @row-click="handleRowClick"
     >
+      <el-table-column type="selection" width="44px"></el-table-column>
       <el-table-column label="用户名" prop="userName" sortable="custom" align="center" width="150px">
         <template slot-scope="{row}">
           <span class="link-type" @click="handleUpdate(row)">{{row.userName}}</span>
@@ -137,21 +134,23 @@
           <span>{{scope.row.phoneNumber}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width">
+      <el-table-column label="操作" align="center" width="125">
         <template slot-scope="{row}">
           <el-button
             type="primary"
             size="mini"
             @click="handleUpdate(row)"
             v-permission="['AbpIdentity.Users.Update']"
-          >编辑</el-button>
+            icon="el-icon-edit"
+          />
           <el-button
             type="danger"
             size="mini"
             @click="handleDelete(row)"
             :disabled="row.userName==='admin'"
             v-permission="['AbpIdentity.Users.Delete']"
-          >删除</el-button>
+            icon="el-icon-delete"
+          />
         </template>
       </el-table-column>
     </el-table>
@@ -167,23 +166,10 @@
 </template>
 
 <script>
-import waves from "@/directive/waves"; // waves directive
 import { isvalidPhone } from "@/utils/validate";
 import Pagination from "@/components/Pagination"; // secondary package based on el-pagination
 import permission from "@/directive/permission/index.js";
 
-const defaultForm = {
-  id: null,
-  username: null,
-  nickName: null,
-  gender: "男",
-  email: null,
-  enabled: "false",
-  roles: [],
-  jobs: [],
-  dept: { id: null },
-  phone: null
-};
 export default {
   name: "User",
   components: { Pagination },
@@ -201,25 +187,29 @@ export default {
     };
     return {
       rules: {
-        username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 2, max: 20, message: "长度在 2 到 20 个字符", trigger: "blur" }
-        ],
-        nickName: [
-          { required: true, message: "请输入用户昵称", trigger: "blur" },
-          { min: 2, max: 20, message: "长度在 2 到 20 个字符", trigger: "blur" }
-        ],
-        email: [
-          { required: true, message: "请输入邮箱地址", trigger: "blur" },
-          { type: "email", message: "请输入正确的邮箱地址", trigger: "blur" }
-        ],
-        phone: [{ required: true, trigger: "blur", validator: validPhone }]
+        // userName: [
+        //   { required: true, message: "请输入用户名", trigger: "blur" },
+        //   { min: 2, max: 20, message: "长度在 2 到 20 个字符", trigger: "blur" }
+        // ],
+        // name: [
+        //   { required: true, message: "请输入用户姓名", trigger: "blur" },
+        //   { min: 2, max: 20, message: "长度在 2 到 20 个字符", trigger: "blur" }
+        // ],
+        // email: [
+        //   { required: true, message: "请输入邮箱地址", trigger: "blur" },
+        //   { type: "email", message: "请输入正确的邮箱地址", trigger: "blur" }
+        // ],
+        // phoneNumber: [
+        //   { required: true, trigger: "blur", validator: validPhone }
+        // ]
       },
       form: {},
       list: null,
-      checkedRoles: [],
+      roleList: [],
+      checkedRole: "",
       totalCount: 0,
       listLoading: true,
+      formLoading: false,
       listQuery: {
         Filter: "",
         Sorting: "",
@@ -228,9 +218,12 @@ export default {
       },
       page: 1,
       dialogFormVisible: false,
+      multipleSelection: [],
+      formTitle: "",
+      isEdit: false
     };
   },
-    created() {
+  created() {
     this.getList();
   },
   methods: {
@@ -243,20 +236,98 @@ export default {
         this.listLoading = false;
       });
     },
+    fetchData(id) {
+      this.$axios.gets("/api/identity/users/" + id).then(response => {
+        this.form = response;
+        this.getAllRoles();
+        this.$axios.gets("/api/identity/users/" + id + "/roles").then(data => {
+          if (data.items) {
+            this.checkedRole = data.items[0].name;
+          }
+        });
+      });
+    },
+    getAllRoles() {
+      this.roleList = [];
+      this.$axios.gets("/api/identity/roles/all").then(response => {
+        response.items.forEach(element => {
+          let options = {};
+          options.value = element.name;
+          options.label = element.name;
+          this.roleList.push(options);
+        });
+      });
+    },
     handleFilter() {
       this.page = 1;
       this.getList();
     },
     resetQuery() {},
-    search() {},
-    cancel() {},
-    save() {},
-    del() {},
-    handleDelete(row){
-
+    save() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.formLoading = true;
+          if (this.checkedRole) {
+            this.form.roleNames = [];
+            this.form.roleNames.push(this.checkedRole);
+          }
+          if (this.isEdit) {
+            this.$axios
+              .puts("/api/identity/users/" + this.form.id, this.form)
+              .then(response => {
+                this.formLoading=false
+                this.$notify({
+                  title: "成功",
+                  message: "更新成功",
+                  type: "success",
+                  duration: 2000
+                });
+              })
+          } else {
+            this.$axios
+              .posts("/api/identity/users", this.form)
+              .then(response => {
+                this.formLoading=false
+                this.$notify({
+                  title: "成功",
+                  message: "新增成功",
+                  type: "success",
+                  duration: 2000
+                });
+              });
+          }
+        }
+      });
     },
-    handleUpdate(row){
-
+    del() {},
+    handleCreate() {
+      this.formTitle = "新增用户";
+      this.isEdit = false;
+      this.form = {};
+      this.checkedRole=""
+      this.dialogFormVisible = true;
+      this.getAllRoles()
+    },
+    handleDelete(row) {},
+    handleUpdate(row) {
+      this.formTitle = "修改用户";
+      this.isEdit = true;
+      
+      if (row) {
+        this.fetchData(row.id);
+        this.dialogFormVisible = true;
+      } else {
+        if (this.multipleSelection.length != 1) {
+          this.$message({
+            message: "编辑必须选择单行",
+            type: "warning"
+          });
+          return;
+        } else {
+          this.fetchData(this.multipleSelection[0].id);
+          this.dialogFormVisible = true;
+        }
+      }
     },
     sortChange(data) {
       const { prop, order } = data;
@@ -266,6 +337,13 @@ export default {
       }
       this.listQuery.Sorting = prop + " " + order;
       this.handleFilter();
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    handleRowClick(row, column, event) {
+      this.$refs.multipleTable.clearSelection();
+      this.$refs.multipleTable.toggleRowSelection(row);
     }
   }
 };
