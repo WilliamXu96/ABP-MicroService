fc5822f28a33f3ee45d5cc5ba3aa72e383ed2c7a
xuchengzhi <1091113110@qq.com>
2020-06-17 10:47:53 +0800

    [日常提交]：修改启动脚本

--- run/run_services.bat
+++ run/run_services.bat
@@ -4,5 +4,8 @@ start dotnet run
 cd ../../IdentityService/IdentityService.Host
 start dotnet run
 
-cd ../../Gateways/WebAppGateway/WebAppGateway.Host
+cd ../../MicroServices/Business/Business.Host
+start dotnet run
+
+cd ../../../Gateways/WebAppGateway/WebAppGateway.Host
 start dotnet run
