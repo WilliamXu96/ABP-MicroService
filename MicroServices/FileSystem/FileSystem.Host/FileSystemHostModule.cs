ad3846c4f6c7bd177d35f46a5b6d95ca5a8bcca0
Boss xu <1091113110@qq.com>
2020-09-20 11:13:13 +0800

    [添加服务]：添加FileSystem服务

---
+++ MicroServices/FileSystem/FileSystem.Host/FileSystemHostModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    public class FileSystemHostModule
+    {
+    }
+}
