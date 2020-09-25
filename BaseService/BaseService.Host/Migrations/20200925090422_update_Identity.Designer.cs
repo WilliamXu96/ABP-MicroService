bfe252e088c9935dc19f8313234bb3162e306667
xuchengzhi <1091113110@qq.com>
2020-09-25 17:57:36 +0800

    [框架升级]：BaseService升级至3.1.0

--- BaseService/BaseService.Host/Migrations/20200925090422_update_Identity.Designer.cs
+++ BaseService/BaseService.Host/Migrations/20200925090422_update_Identity.Designer.cs
@@ -4,15 +4,17 @@ using BaseService.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace BaseService.Migrations
 {
     [DbContext(typeof(BaseServiceMigrationDbContext))]
-    partial class BaseServiceMigrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200925090422_update_Identity")]
+    partial class update_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
