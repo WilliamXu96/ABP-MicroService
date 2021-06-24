ff8a30137e8bff5da74ba58fd500a96c9fefd566
xuchengzhi <1091113110@qq.com>
2021-06-24 14:07:55 +0800

    [日常提交]：移除AbpUsers表OrgId字段，添加base_user_orgs表

--- BaseService/BaseService.Host/Migrations/20210624060445_add_UserOrganization.Designer.cs
+++ BaseService/BaseService.Host/Migrations/20210624060445_add_UserOrganization.Designer.cs
@@ -4,15 +4,17 @@ using BaseService.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace BaseService.Migrations
 {
     [DbContext(typeof(BaseServiceMigrationDbContext))]
-    partial class BaseServiceMigrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210624060445_add_UserOrganization")]
+    partial class add_UserOrganization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BaseService/BaseService.Host/Migrations/20210624060445_add_UserOrganization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseService.Migrations
+{
+    public partial class add_UserOrganization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrgId",
+                table: "AbpUsers");
+
+            migrationBuilder.CreateTable(
+                name: "base_user_orgs",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrganizationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_base_user_orgs", x => new { x.UserId, x.OrganizationId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "base_user_orgs");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrgId",
+                table: "AbpUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
