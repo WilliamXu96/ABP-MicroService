2a98c3bc2551e68bdae8289874ae09e4802f898d
xuchengzhi <1091113110@qq.com>
2021-06-29 09:51:57 +0800

    [框架调整]：移除Identity代码文件

--- AbpModules/Identity/Volo.Abp.Identity.Domain.Shared/Volo/Abp/Identity/IdentityErrorCodes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Volo.Abp.Identity
-{
-    public static class IdentityErrorCodes
-    {
-        public const string UserSelfDeletion = "Volo.Abp.Identity:010001";
-        public const string MaxAllowedOuMembership = "Volo.Abp.Identity:010002";
-    }
-}
