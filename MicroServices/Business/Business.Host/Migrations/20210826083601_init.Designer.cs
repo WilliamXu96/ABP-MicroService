118ddb79714ea1cb5a298cd7528c4a7deac987c6
xuchengzhi <1091113110@qq.com>
2021-08-26 16:37:27 +0800

    [日常提交]：更新Business服务数据迁移

--- MicroServices/Business/Business.Host/Migrations/20210826083601_init.Designer.cs
+++ MicroServices/Business/Business.Host/Migrations/20210826083601_init.Designer.cs
@@ -4,15 +4,17 @@ using Business.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace Business.Migrations
 {
     [DbContext(typeof(BusinessMigrationDbContext))]
-    partial class BusinessMigrationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210826083601_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
