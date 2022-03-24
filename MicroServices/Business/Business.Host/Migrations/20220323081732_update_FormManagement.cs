bb39c664f666b3a66405ad13056bc4c8381beb43
xuchengzhi <1091113110@qq.com>
2022-03-24 10:11:43 +0800

    [日常提交]：更新Business包版本

---
+++ MicroServices/Business/Business.Host/Migrations/20220323081732_update_FormManagement.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Business.Migrations
+{
+    public partial class update_FormManagement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Span",
+                table: "base_form_fields",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Span",
+                table: "base_form_fields");
+        }
+    }
+}
