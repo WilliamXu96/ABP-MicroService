18da66d724132ada65b0001a31fe550dbdd6741b
xuchengzhi <1091113110@qq.com>
2022-09-17 17:20:31 +0800

    [优化配置]：解决webapp无法正常下载，提供者：DataAnge

--- WebApp/vue/src/components/MarkdownEditor/index.vue
+++ WebApp/vue/src/components/MarkdownEditor/index.vue
@@ -4,13 +4,15 @@
 
 <script>
 // deps for editor
-import 'codemirror/lib/codemirror.css' // codemirror
-import 'tui-editor/dist/tui-editor.css' // editor ui
-import 'tui-editor/dist/tui-editor-contents.css' // editor content
 
-import Editor from 'tui-editor'
+
+
+
+
+import 'codemirror/lib/codemirror.css'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import Editor from '@toast-ui/editor'
 import defaultOptions from './default-options'
-
 export default {
   name: 'MarkdownEditor',
   props: {
@@ -62,8 +64,8 @@ export default {
   },
   watch: {
     value(newValue, preValue) {
-      if (newValue !== preValue && newValue !== this.editor.getValue()) {
-        this.editor.setValue(newValue)
+      if (newValue !== preValue && newValue !== this.editor.getMarkdown()) {
+        this.editor.setMarkdown(newValue)
       }
     },
     language(val) {
@@ -90,10 +92,10 @@ export default {
         ...this.editorOptions
       })
       if (this.value) {
-        this.editor.setValue(this.value)
+        this.editor.setMarkdown(this.value)
       }
       this.editor.on('change', () => {
-        this.$emit('input', this.editor.getValue())
+        this.$emit('input', this.editor.getMarkdown())
       })
     },
     destroyEditor() {
@@ -101,11 +103,11 @@ export default {
       this.editor.off('change')
       this.editor.remove()
     },
-    setValue(value) {
-      this.editor.setValue(value)
+    setMarkdown(value) {
+      this.editor.setMarkdown(value)
     },
-    getValue() {
-      return this.editor.getValue()
+    getMarkdown() {
+      return this.editor.getMarkdown()
     },
     setHtml(value) {
       this.editor.setHtml(value)
