500873a420e20263a47fe47eff6c07f57e063450
xuchengzhi <1091113110@qq.com>
2022-10-19 14:01:34 +0800

    [框架调整]：添加OpenIddic

---
+++ AuthServer/OpenIddict/AuthServer.Host/BrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace AuthServer;
+
+[Dependency(ReplaceServices = true)]
+public class AuthServerBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "AuthServer";
+}
---
+++ AuthServer/OpenIddict/AuthServer.Host/EntityFrameworkCore/AuthServerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.OpenIddict.EntityFrameworkCore;
+
+namespace AuthServer.EntityFrameworkCore
+{
+    public class AuthServerDbContext : AbpDbContext<AuthServerDbContext>
+    {
+        public AuthServerDbContext(DbContextOptions<AuthServerDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ConfigureOpenIddict();
+        }
+    }
+}
