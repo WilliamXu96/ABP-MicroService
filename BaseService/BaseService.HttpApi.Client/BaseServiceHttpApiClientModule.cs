7cae913683978fedaebe2bd399d2c00703579ebd
xuchengzhi <1091113110@qq.com>
2022-11-28 14:32:44 +0800

    [日常提交]：添加BaseServiceHttpApiClientModule

---
+++ BaseService/BaseService.HttpApi.Client/BaseServiceHttpApiClientModule.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Http.Client;
+using Volo.Abp.Http.Client.IdentityModel;
+using Volo.Abp.Http.Client.IdentityModel.Web;
+using Volo.Abp.Modularity;
+
+namespace BaseService.HttpApi.Client
+{
+    [DependsOn(
+        typeof(BaseServiceApplicationContractsModule),
+        typeof(AbpHttpClientIdentityModelModule),
+        typeof(AbpHttpClientIdentityModelWebModule)
+       
+)]
+    public class BaseServiceHttpApiClientModule : AbpModule
+    {
+        public const string RemoteServiceName = "BaseService";
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddHttpClientProxies(
+                typeof(BaseServiceApplicationContractsModule).Assembly,
+                RemoteServiceName
+            );
+        }
+    }
+}
